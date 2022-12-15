e1345e6a0750f9aeae33e250ebf03412e0f4cea8
Samuel Gaddis <siddag14@live.com>
2022-12-15 14:33:09 -0500

    commit tree b9303d79ed9481044c149c1a46faff543fa6376f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    # render json: students
+    
+    # students = if params[:name]
+    #   Student.find_by(params[:name])
+    # else
+    #   Student.All 
+    # end
+    # render json: students 
+    byebug
   end
+ 
+
+  def show 
+    student = Student.find_by(id: params[:id])
+    render json: student 
+  end
+
 
 end
